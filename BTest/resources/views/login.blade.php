4e11e9c3c49171103fa2e34f017d4bfb1ee589b8
Mr.Hugo <mosieur.hugo@gmail.com>
2017-07-05 01:06:08 +0200

    remove useless connexion blade and update login.blade style

--- BTest/resources/views/login.blade.php
+++ BTest/resources/views/login.blade.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Wat-izz-Dis - Connexion</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+    <link rel="stylesheet" href="{{ asset("../bootstrap/css/bootstrap.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("../bootstrap/css/index.css") }}">
 </head>
 <body>
 <center>
@@ -20,12 +21,27 @@
             echo '<label>Error: invalid identifier</label>';
         }
     }?>
-    <form action="loginme" method="post">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        EMAIL : <input type="text" name="email"><br/>
-        PASSWORD : <input type="password" name="password"><br/>
-        <input type="submit" name="login" value="Login">
-    </form>
+    <h1 class="text-center">Se connecter</h1>
+    <div class="text-center">
+        <div class="row">
+        </div>
+        <br />
+        <div>
+            <div class="col-md-offset-2 col-md-8">
+                <form action="loginme" method="post">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="row">
+                        <div class="col-md-offset-2 col-md-8">
+                          Email : <input style="width: 80%; margin: auto" placeholder="Email..." class="form-control" type="text" name=""><br/>
+                          Mot de passe : <input style="width: 80%; margin: auto" placeholder="Mot de passe..." class="search_input form-control" name="" /></br>
+                        </div>
+                    </div>
+                    <br />
+                    <input class="btn btn-success btn-lg" type="submit" name="login" value="Login">
+                </form>
+            </div>
+        </div>
+    </div>
 </center>
 </body>
 </html>
